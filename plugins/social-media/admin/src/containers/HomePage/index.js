49d8b29106a3755c472b83cf4b028b9fd96754dc
Nirmal Khedkar <nirmalhk7@gmail.com>
2021-04-14 14:00:56 +0530

    feat(about-club): about Club Schema

--- plugins/social-media/admin/src/containers/HomePage/index.js
+++ plugins/social-media/admin/src/containers/HomePage/index.js
@@ -1,8 +1,8 @@
-/*
- *
- * HomePage
- *
- */
+import { Button, Padded, Text } from "@buffetjs/core";
+import { Header } from "@buffetjs/custom";
+import { LoadingBar } from "@buffetjs/styles";
+import { faUpload } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import React, { memo } from 'react';
 // import PropTypes from 'prop-types';
