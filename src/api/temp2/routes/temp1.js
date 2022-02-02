1705bfc831d93beff86cd55574ac2eb255af9571
Nirmal Khedkar <nirmalhk7@gmail.com>
2022-02-02 23:14:13 +0300

    Status Update

---
+++ src/api/temp2/routes/temp1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * temp2 router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::temp2.temp1');
---
+++ src/api/temp2/services/temp1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * temp2 service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::temp2.temp1');
